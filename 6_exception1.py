4c0479f0cde52f6a69bc8a3d94731a2f0ac44fcc
Cosmmoss <pavelandreevich16@gmail.com>
2024-05-27 14:00:56 +0300

    did the 6th task

--- 6_exception1.py
+++ 6_exception1.py
@@ -14,7 +14,15 @@ def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
+  
+    while True:
+        try:
+            if input('Как дела? ') == 'Хорошо':
+                print('Ну хорошо!')
+                break
+        except KeyboardInterrupt:
+            print('Пока!')
+            break
     
 if __name__ == "__main__":
     hello_user()
